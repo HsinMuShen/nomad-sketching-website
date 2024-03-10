567a784882f9b366f4ce786d4a27319a5698d770
michael_shen <electrycity84@gmail.com>
2024-03-10 16:43:54 +0800

    feat: setup useUpdateArticle hooks

--- components/admin/hooks/use-article/index.ts
+++ components/admin/hooks/use-article/index.ts
@@ -1,6 +1,7 @@
 import type { CreateArticleType } from 'components/admin/types'
 import { useState, useCallback } from 'react'
 import useCreateArticle from './use-create-article'
+import useUpdateArticle from './use-update-article'
 
 const useArticle = () => {
   const [article, setArticle] = useState<CreateArticleType>({
@@ -9,15 +10,24 @@ const useArticle = () => {
   })
 
   const { createArticle: createNewArticle } = useCreateArticle()
+  const { updateArticle: updateArticleData } = useUpdateArticle()
 
   const createArticle = useCallback(() => {
     createNewArticle(article)
   }, [article, createNewArticle])
 
+  const updateArticle = useCallback(
+    (id: string) => {
+      updateArticleData(id, article)
+    },
+    [article, updateArticleData],
+  )
+
   return {
     article,
     setArticle,
     createArticle,
+    updateArticle,
   }
 }
 
