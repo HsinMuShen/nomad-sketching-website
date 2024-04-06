e145e5da2921fc8c8d0f087791a82cdecd99ad34
michael_shen <electrycity84@gmail.com>
2024-04-06 18:05:24 +0800

    feat: setup tiptap

--- components/admin/index.tsx
+++ components/admin/index.tsx
@@ -1,7 +1,13 @@
 import Articles from './components/Articles'
+import MessageInput from 'components/common/MessageInput'
 
 const Admin = () => {
-  return <Articles />
+  return (
+    <>
+      <Articles />
+      <MessageInput />
+    </>
+  )
 }
 
 export default Admin
