184e9a980835b68a4d813241d2bf7082e64cfc1e
michael_shen <electrycity84@gmail.com>
2024-04-28 19:22:44 +0800

    feat: setup header component

---
+++ components/Layout/components/Header/index.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import Link from 'next/link'
+import { Icon } from '@ui'
+import { NAV_LINKS, SIDEBAR_LINKS } from './constants'
+
+type HeaderProps = {
+  isAdmin: boolean
+}
+
+const Header = ({ isAdmin }: HeaderProps) => {
+  const [isSideBar, setIsSideBar] = useState(false)
+  const desktopClass = 'mx-5 text-gray-800 hover:text-gray-400 hidden md:block'
+
+  return (
+    <div className="fixed w-full h-15 bg-white flex justify-between items-center border-b-2 top-0 z-10">
+      <div className="flex items-center">
+        <Link href="/" className="flex items-center mx-5 text-gray-800 hover:text-gray-400 font-800">
+          <Icon className="mr-2" icon="i-mdi-grease-pencil" size="xl" />
+          {'Nomad Sketching'}
+        </Link>
+        {NAV_LINKS.map((link) => (
+          <Link key={link.label} href={link.href} className={desktopClass}>
+            {link.label}
+          </Link>
+        ))}
+      </div>
+      {isAdmin ? (
+        <Link href="/admin" className={`${desktopClass}`}>
+          <Icon icon="i-mdi-account-tie-hat" size="3xl" />
+        </Link>
+      ) : (
+        <Link href="/login" className={`${desktopClass}`}>
+          <Icon icon="i-mdi-login" size="3xl" />
+        </Link>
+      )}
+      <div className={`flex items-center md:hidden`}>
+        <p className="mx-5 text-gray-800 cursor-pointer hover:text-gray-600" onClick={() => setIsSideBar(true)}>
+          <Icon icon="i-mdi-menu" size="xl" />
+        </p>
+      </div>
+      <HeaderSidebar isSideBar={isSideBar} setIsSideBar={setIsSideBar} />
+    </div>
+  )
+}
+
+export default Header
+
+type HeaderSidebarProps = {
+  isSideBar: boolean
+  setIsSideBar: (value: boolean) => void
+}
+
+const HeaderSidebar = ({ isSideBar, setIsSideBar }: HeaderSidebarProps) => {
+  return (
+    <div
+      className={`fixed right-0 top-0 h-screen w-50 bg-black bg-opacity-60 transition-right duration-800 ${isSideBar ? 'right-0' : '-right-200'}`}
+    >
+      <p className="absolute top-4 left-5 text-white cursor-pointer" onClick={() => setIsSideBar(false)}>
+        <Icon icon="i-mdi-close" size="xl" />
+      </p>
+      <div className="mt-20 flex flex-col">
+        {SIDEBAR_LINKS.map((link) => (
+          <Link key={link.label} href={link.href} className="mx-5 my-1 text-white">
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from 'react'
-import Link from 'next/link'
+import Header from './components/Header'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-80vw px-4 my-12 mx-auto">
-      {children}
-      <div>
-        <Link href="/">Back to home</Link>
-      </div>
+    <div className="w-full">
+      <Header isAdmin={true} />
+      <div className="max-w-80vw px-4 my-18 mx-auto">{children}</div>
     </div>
   )
 }
