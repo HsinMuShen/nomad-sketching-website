72905f118e018b0579ec64932804b31ce71ea10a
michael_shen <electrycity84@gmail.com>
2024-05-05 17:03:29 +0800

    feat: install unocss eslint-config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import type { AppProps } from 'next/app'
+import 'src/styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
