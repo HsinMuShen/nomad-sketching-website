321d735d38c2bfc54bd2f027d32e84b57c514adc
michael_shen <electrycity84@gmail.com>
2024-06-23 01:18:25 +0800

    feat: setup headers

--- src/components/Layout/components/Header/constants/index.ts
+++ src/components/Layout/components/Header/constants/index.ts
@@ -1,6 +1,12 @@
 export const NAV_LINKS = [
   { href: '/artworks', label: 'Artworks' },
-  { href: '/about', label: 'About Me' },
+  // { href: '/about', label: 'About Me' },
 ]
 
-export const SIDEBAR_LINKS = [...NAV_LINKS, { href: '/admin', label: 'Admin' }, { href: '/', label: 'Logout' }]
+export const SIDEBAR_LINKS = [
+  ...NAV_LINKS,
+  { href: '/about', label: 'About Me' },
+  { href: '/special-thanks', label: 'Special Thanks' },
+  // { href: '/admin', label: 'Admin' },
+  // { href: '/', label: 'Logout' }
+]
