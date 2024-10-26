52f9925134b8385c95238d4315ca98f11a89285c
michael_shen <electrycity84@gmail.com>
2024-10-27 00:05:50 +0800

    feat: add text-ellipsis setting on title

--- src/components/Diaries/User/components/diaries/index.tsx
+++ src/components/Diaries/User/components/diaries/index.tsx
@@ -35,7 +35,7 @@ const Diary = ({ diary }: DiaryProps) => {
     <Link href={`/diary/${id}`}>
       <div className="w-60 my-2" key={id}>
         <div className="flex justify-between items-end mb-1">
-          <div className="text-4 font-bold">{title}</div>
+          <div className="text-4 font-bold text-ellipsis whitespace-nowrap overflow-hidden h-7 max-w-35">{title}</div>
           <div className="text-3 mr-2">{createdAt && getCreatedTime(createdAt)}</div>
         </div>
         <div className="relative border-1 h-40 w-full overflow-hidden">
